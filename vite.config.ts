92b11a6e8ab11cb9969778835ba477fe94b893ca
ahmed <wezonaser50@gmail.com>
2023-09-04 11:03:27 +0300

    Adding final dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/ahmed-ayob-portofolio-with-ts/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html',
+                about: './src/pages/aboutPage/index.html',
+                works: './src/pages/works/index.html',
+                contact: './src/pages/contactPage/index.html',
+            },
+        },
+    },
 })
